c01f491ae9f14e1dd3bf6020b2cabc3cd525ddd3
Youssef Elshahat <Ahmedyousef783@gmail.com>
2024-12-18 16:53:07 +0200

    adding category class + contrller + dbcontext+categoryconfig + Di In program.cs

---
+++ JOStore/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using JOStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JOStore.Data
+{
+    public class AppDbContext :DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options) 
+        {
+
+                
+        }
+        public DbSet<Category> Categories { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+        }
+
+    }
+}
---
+++ JOStore/Data/Config/CategoryConfig.cs
@@ -0,0 +1,36 @@
+﻿using JOStore.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace JOStore.Data.DataConfig
+{
+    public class CategoryConfig : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x =>x.Id )
+                .IsRequired().ValueGeneratedNever();
+
+            builder.Property(x => x.Name)
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.ToTable("Categories");
+
+            builder.HasData(LoadCategories());
+                
+        }
+
+        private static List<Category> LoadCategories()
+        {
+            return new List<Category>
+            {
+                new Category { Id = 1,Name = "MOTHERBOARD" },
+                new Category { Id = 2,Name = "PROCESSOR" },
+                new Category { Id = 3,Name = "GRAPHICS CARD" }
+            };
+        }
+    }
+}
