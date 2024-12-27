d33a603c2d8357e3c84b1d48ca042ed6874f33ce
Youssef Elshahat <Ahmedyousef783@gmail.com>
2024-12-27 16:46:59 +0200

    Finish Product CRUD

--- JOStore/Areas/Admin/Controllers/ProductController.cs
+++ JOStore/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using Store.DataAccess.Repository;
 using Store.DataAccess.Repository.IRepository;
 using Store.Models;
 using Store.Models.ViewModels;
@@ -19,19 +21,19 @@ namespace JOStore.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Product> productList = _iUnitOfWork
-                .Product.GetAll(includeProperties:"Category").ToList();
+                .Product.GetAll(includeProperties: "Category").ToList();
             return View(productList);
         }
 
-        public IActionResult Upsert(int? Id )
+        public IActionResult Upsert(int? Id)
         {
             ProductVM productVM = new ProductVM()
             {
                 CategoryList = GetCategoryList(),
                 Product = new Product()
-    
+
             };
-            if (Id == null || Id == 0) 
+            if (Id == null || Id == 0)
             {
                 //Create
                 return View(productVM);
@@ -46,15 +48,15 @@ namespace JOStore.Areas.Admin.Controllers
             }
         }
         [HttpPost]
-        public IActionResult Upsert(Product product,IFormFile? file)
+        public IActionResult Upsert(Product product, IFormFile? file)
         {
-            
+
             if (ModelState.IsValid)
             {
                 string wwwRootPath = _webHostEnvironment.WebRootPath;
-                if (file != null) 
+                if (file != null)
                 {
-                    string fileName = Guid.NewGuid().ToString() 
+                    string fileName = Guid.NewGuid().ToString()
                         + Path.GetExtension(file.FileName);
                     string productPath = Path.Combine(wwwRootPath, "Images", "Product");
                     if (!string.IsNullOrEmpty(product.ImageUrl))
@@ -65,7 +67,7 @@ namespace JOStore.Areas.Admin.Controllers
                         {
                             System.IO.File.Delete(oldImagePath);
                         }
-                        
+
                     }
 
                     using (var fileStream = new FileStream(Path.
@@ -74,10 +76,10 @@ namespace JOStore.Areas.Admin.Controllers
                         file.CopyTo(fileStream);
 
                     }
-                     product.ImageUrl = "/Images/Product/" + fileName;
+                    product.ImageUrl = "/Images/Product/" + fileName;
 
                 }
-                if(product.Id == 0)
+                if (product.Id == 0)
                 {
                     _iUnitOfWork.Product.Add(product);
 
@@ -104,42 +106,7 @@ namespace JOStore.Areas.Admin.Controllers
 
         }
 
-        
-        public IActionResult Delete(int? Id)
-        {
-            if (Id.HasValue == false)
-            {
-                return NotFound();
-            }
 
-            Product? productFromDb = _iUnitOfWork.Product.Get(x => x.Id == Id);
-
-
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-
-            return View(productFromDb);
-
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken] // Prevent CSRF attacks
-        [ActionName("Delete")]
-        public IActionResult DeleteConfirmed(int id)
-        {
-            var product = _iUnitOfWork.Product.Get(x => x.Id == id);
-
-            if (product == null)
-            {
-                return NotFound();
-            }
-
-            _iUnitOfWork.Product.Delete(product);
-            _iUnitOfWork.Save();
-            TempData["Alert"] = "Product Deleted Successfully";
-            return RedirectToAction("Index", "Product");
-        }
         private IEnumerable<SelectListItem> GetCategoryList()
         {
             return _iUnitOfWork.Category.GetAll().Select(u => new SelectListItem
@@ -149,5 +116,51 @@ namespace JOStore.Areas.Admin.Controllers
             });
         }
 
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var products = _iUnitOfWork.Product.GetAll(includeProperties: "Category")
+                .Select(product => new
+                {
+                    name = product.Name,
+                    price = product.Price,
+                    description = product.Description,
+                    category = product.Category?.Name, // Avoid full Category object
+                    imageUrl = product.ImageUrl,
+                    id = product.Id,
+
+                });
+
+            return Json(new { data = products });
+        }
+        [HttpDelete]    
+        public IActionResult Delete(int? id)
+        {
+            var productToBeDeleted = _iUnitOfWork.Product.Get(x => x.Id == id);
+            if (productToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error While Deleting" });
+            }
+
+            //Delete the old image
+            var oldImagePath = Path.
+            Combine(_webHostEnvironment.WebRootPath,
+            productToBeDeleted.ImageUrl.TrimStart('/')
+            .Replace('/', Path.DirectorySeparatorChar));
+
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+            _iUnitOfWork.Product.Delete(productToBeDeleted);
+            _iUnitOfWork.Save();
+            return Json(new { success = true, message = "Delet Success" });
+
+
+            #endregion
+        }
     }
-}
+
+    }
--- Store.Models/Product.cs
+++ Store.Models/Product.cs
@@ -11,7 +11,7 @@ namespace Store.Models
 {
     public class Product
     {
-        [Range(1, int.MaxValue, ErrorMessage = "Id Must Be From 1 to 2147483647")]
+        //[Range(1, int.MaxValue, ErrorMessage = "Id Must Be From 1 to 2147483647")]
         public int Id { get; set; }
         [DisplayName("Product Name")]
         public string? Name { get; set; }
